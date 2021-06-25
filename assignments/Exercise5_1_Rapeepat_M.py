6d345896ad1a454ec216e8fc1d5e76ff6ddce32d
nuengpond <85215242+nuengpond@users.noreply.github.com>
2021-06-25 11:29:21 +0700

    Delete Exercise5_1_Rapeepat_M.py

--- assignments/Exercise5_1_Rapeepat_M.py
+++
@@ -1,6 +0,0 @@
-num1 = int(input("Number1 :"))
-num2 = int(input("Number2 :"))
-print(num1,"+",num2,"=",num1+num2)
-print(num1,"-",num2,"=",num1-num2)
-print(num1,"*",num2,"=",num1*num2)
-print(num1,"/",num2,"=",num1/num2)
