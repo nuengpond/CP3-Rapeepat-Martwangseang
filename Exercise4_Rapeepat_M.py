31d4e2ce7a02c22b7b449f49544bf722db55c395
nuengpond <85215242+nuengpond@users.noreply.github.com>
2021-06-25 11:12:25 +0700

    Create Exercise4_Rapeepat_M.py

---
+++ Exercise4_Rapeepat_M.py
@@ -0,0 +1,9 @@
+FE = 80.5
+GB = 79.0
+ICS = 88.0
+CP = 74.3
+print("---- Your Score ----")
+print("Foundation English : ", FE)
+print("General Business : ", GB)
+print("Introduction to Computer Systems : ", ICS)
+print("Computer Programming : ", CP)
