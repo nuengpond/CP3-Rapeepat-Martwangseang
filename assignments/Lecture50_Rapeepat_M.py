5840bc3bcfa3df4e58f55bcb431a98ffcf0a100e
nuengpond <85215242+nuengpond@users.noreply.github.com>
2021-06-30 10:58:18 +0700

    Create Lecture50_Rapeepat_M.py

---
+++ assignments/Lecture50_Rapeepat_M.py
@@ -0,0 +1,12 @@
+def PlusNumber(x,y):
+    print("Plus :",x+y)
+def MinusNumber(x, y):
+    print("Minus :",x-y)
+def MultipliedNumber(x, y):
+    print("Multiplied :", x * y)
+def DivideNumber(x, y):
+    print("Divide :", x / y)
+PlusNumber(10,20)
+MinusNumber(10,20)
+MultipliedNumber(10,20)
+DivideNumber(10,20)
